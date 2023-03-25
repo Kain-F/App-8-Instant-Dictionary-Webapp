b531a7377c3c9e608764b69c9de30587450ed93b
kain <kfontenai@gmail.com>
2023-03-25 16:03:06 +0100

    on click event handling

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ def first_page():
     #to style our components we use the classes parameter and more information can be found on tailwindcss
     #in order to get the general background styling we need to put the components inside the div that defines the background
 
-    jp.Input(a=div1,placeholder = 'Enter first value',classes = 'form-input')
-    jp.Input(a=div1,placeholder = 'Enter second value',classes = 'form-input')
-    jp.Div(a=div1,text='Result goes here...')
+    in_1= jp.Input(a=div1,placeholder = 'Enter first value',classes = 'form-input')
+    in_2= jp.Input(a=div1,placeholder = 'Enter second value',classes = 'form-input')
+    d_output =jp.Div(a=div1,text='Result goes here...')
 
 
     # in order to put our components next to each other we will make a grid for them
@@ -31,6 +31,7 @@ def first_page():
     div2 = jp.Div(a=div, classes='grid grid-cols-2 gap-4 p-4 border border-black')
 
     jp.Button(a=div2,text = 'Calculate',click = sum_up,
+              in1 = in_1, in2 = in_2, d = d_output,
               # now we will make a border around our button
               classes = 'border border-blue-500 m-2 py-1 px-4 rounded text-blue-600 '
                         'hover:bg-red-500 hover:text-white')
@@ -40,7 +41,11 @@ def first_page():
 
 # in order to execute the code on the click event we need to add widget and msg params in the function
 def sum_up(widget,msg):
-    print('Clicked')
+    input1 = widget.in1.value
+    input2 = widget.in2.value
+    result = float(input1) + float(input2)
+    widget.d.text = result
+
 
 
 
