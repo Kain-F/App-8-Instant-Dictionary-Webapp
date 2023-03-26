0d03d24edc29da2287156906e8e3f8aa32c8ed1d
kain <kfontenai@gmail.com>
2023-03-26 13:28:15 +0200

    created navigation menu on the about page

--- about.py
+++ about.py
@@ -15,6 +15,20 @@ class About():
         toolbar = jp.QToolbar(a=header,title='Instant dictionary')
         drawer = jp.QDrawer(a = layout,show_if_above = True, dense='True',v_model = 'leftDrawerOpen',
                             side = 'left', border = True)
+
+        #now we will populate our drawer with the links to the other pages
+        #you can do this by using jp.A()
+
+        scroll_area= jp.QScrollArea(a=drawer,classes = 'fit')
+        qlist= jp.QList(a=scroll_area)
+        a_classes = 'p-2 m-2 text-lg text-blue-400 hover:text-blue-700'
+        jp.A(a=qlist,text = 'Home', href = '/home', classes = a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist,text = 'Dictionary', href = '/dictionary',classes = a_classes)
+        jp.Br(a=qlist)
+        jp.A(a=qlist,text = 'About', href = '/about',classes = a_classes)
+        jp.Br(a=qlist)
+
         jp.QBtn(a = toolbar,dense=True,flat=True,round=True,icon='menu',bordered=True,
                  drawer=drawer,click = cls.move_drawer)
         jp.QToolbar(a=toolbar,text='Instant dictionary')
